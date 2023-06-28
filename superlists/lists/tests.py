39ba5ad4f69acc612f40ceacc1236daf4cd50108
gusevskiy <vlgu@mail.ru>
2023-06-28 13:45:39 +0300

    stop

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Item
+from .models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -10,53 +10,78 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        """Тест: можно сохранить post-запрос"""
-        self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
+    # def test_can_save_a_POST_request(self):
+    #     """Тест: можно сохранить post-запрос"""
+    #     self.client.post('/', data={'item_text': 'A new list item'})
+    #     self.assertEqual(Item.objects.count(), 1)
+    #     new_item = Item.objects.first()
+    #     self.assertEqual(new_item.text, 'A new list item')
 
-    def test_redirect_after_POST(self):
-        """Тест: переадресует после post-запроса"""
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+    # def test_redirect_after_POST(self):
+    #     """Тест: переадресует после post-запроса"""
+    #     response = self.client.post('/', data={'item_text': 'A new list item'})
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         """Тест: сохраняет элементы, только когда нужно"""
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_item(self):
-        """Тест: отображаются все элементы списка"""
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+    # def test_displays_all_list_item(self):
+    #     """Тест: отображаются все элементы списка"""
+    #     Item.objects.create(text='itemey 1')
+    #     Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
-        print(response.content)
+        # response = self.client.get('/')
+        # print(response.content)
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        # self.assertIn('itemey 1', response.content.decode())
+        # self.assertIn('itemey 2', response.content.decode())
 
 
-class ItemModelTest(TestCase):
-    """Тест модели элемента списка"""
-
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
-        """Тест сохранения и получения элементов списка"""
+        list_ = List()
+        list_.save()
+
         first_item = Item()
-        first_item.text = 'The first (ever) list item'
+        first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
-        second_item.text = 'Item the second'
+        second_item.text = "Item the second"
+        second_item.list = list_
         second_item.save()
 
-        saved_item = Item.objects.all()
-        self.assertEqual(saved_item.count(), 2)
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
-        first_saved_item = saved_item[0]
-        second_saved_item = saved_item[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, list_)
+
+
+class ListViewTest(TestCase):
+    
+    def test_users_list_template(self):
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+
+    #     response = self.client.get('/lists/the-only-list-in-the-world/')
+
+    #     self.assertContains(response, 'itemey 1')  
+    #     self.assertContains(response, 'itemey 2')
--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,13 +1,7 @@
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-
-from django.urls import path
-from lists.views import home_page
+from django.urls import path, include
+from lists import views
 
 urlpatterns = [
-    path("", home_page, name="home"),
+    path("", views.home_page, name='home'),
+    path("lists/", include("lists.urls")),
 ]
